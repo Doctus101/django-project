879ef5f014f943de0ac0dd84750a530f9e55a065
Doctus101 <105364847+Doctus101@users.noreply.github.com>
2022-06-19 13:51:22 +0100

    Add files via upload

---
+++ I4G014742LNJ/Blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class title(models.Model):
+        your_Title = models.charfield(max_length=260) 
+        Author = models.get_user
+        Created_date = models.DateTimeField
+        published_date= models.DateTimeField
+
